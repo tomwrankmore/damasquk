ded4bf2f963f6ad46fd0a04905738f1b6b150abd
Tom <tomwrankmore@gmail.com>
2021-03-24 17:48:09 +0000

    bluergh

--- web/src/components/Background-logo-absolute.js
+++ web/src/components/Background-logo-absolute.js
@@ -1,27 +1,15 @@
 import React, { useRef, useEffect } from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-import { gsap } from 'gsap';
 import styles from './Background-logo-absolute.module.css';
 
 export function BackgroundLogoAbs() {
-  const logoRef = useRef(null);
-
-  useEffect(() => {
-    gsap.from(logoRef.current, {
-      duration: 2.5,
-      autoAlpha: 0,
-      x: 30,
-      ease: 'none',
-    });
-  }, []);
-
   return (
     <StaticImage
       src="../assets/images/logo-mark-truncated.svg"
       alt="Big Icon"
       placeholder="none"
       layout="fullWidth"
-      //   width={450}
+      width={700}
       className={styles.BackgroundLogoAbs}
     />
   );
